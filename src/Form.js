42219c3e5aed738c382023ec56c0782f8de7316f
0xbc614e <0xbc614e@gmail.com>
2020-10-30 17:14:26 +0900

    Redefine to new format

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,3 @@
-import {horizontalAttributes} from './fields.json';
-
 // 이게 진짜 필요함? 지금 용도가 거 getter 붙여주는 거 말고 없는데
 // 아니면 attribute 하나가 바뀌면 score도 바뀌니까 이대로 괜찮다?
 export default class Form {
@@ -14,11 +12,7 @@ export default class Form {
     }
 
     get totalScore() {
-        return horizontalAttributes.reduce((prev, curr) => prev + this[curr], 0);
-    }
-
-    get defects() {
-        return this.defectsNumOfCups * this.defectsIntensity;
+        return this.total;
     }
 
     get finalScore() {
